45b14822044798ed6adcb61d832ee4fad96e670c
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-07-28 15:05:58 +0530

    fixes: staging fixes

--- public/javascripts/form/submitForm.js
+++ public/javascripts/form/submitForm.js
@@ -12,6 +12,29 @@ function submitForm(isModal,source, targetID) {
             statusCode: {
                 400: function (data) {
                     result.html(data.responseText);
+                    function clearFormElements(target_id) {
+                        jQuery("#"+target_id).find(':input').each(function() {
+                            switch(this.type) {
+                                case 'password':
+                                case 'text':
+                                case 'textarea':
+                                case 'file':
+                                case 'select-one':
+                                case 'select-multiple':
+                                case 'date':
+                                case 'number':
+                                case 'tel':
+                                case 'email':
+                                    jQuery(this).val('');
+                                    break;
+                                case 'checkbox':
+                                case 'radio':
+                                    this.checked = false;
+                                    break;
+                            }
+                        });
+                    }
+                    clearFormElements("formBody");
                     $(".error").closest("dd").prev().find('input').css({"border-color":"var(--error)"});
                 },
                 401: function (data) {
