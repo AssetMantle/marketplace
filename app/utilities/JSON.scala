db7f606a2a7713187588a113799de0103e4c5d83
avkr003 <abhinav@persistence.one>
2022-08-18 17:52:29 +0530

    Adding MicroNumber

--- app/utilities/JSON.scala
+++ app/utilities/JSON.scala
@@ -2,9 +2,13 @@ package utilities
 
 import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.databind.JsonMappingException
+import constants.Response.Failure
 import exceptions.BaseException
 import play.api.Logger
 import play.api.libs.json._
+import play.api.libs.ws.WSResponse
+
+import scala.concurrent.{ExecutionContext, Future}
 
 object JSON {
 
@@ -24,4 +28,21 @@ object JSON {
         throw new BaseException(constants.Response.JSON_MAPPING_EXCEPTION)
     }
   }
+
+  def getResponseFromJson[T](response: Future[WSResponse])(implicit exec: ExecutionContext, logger: Logger, module: String, reads: Reads[T]): Future[T] = {
+    response.map { response =>
+      Json.fromJson[T](response.json) match {
+        case JsSuccess(value: T, _: JsPath) => value
+        case jsError: JsError => logger.debug(response.json.toString())
+          logger.error(jsError.toString)
+          throw new BaseException(new Failure(jsError.toString, null))
+      }
+    }.recover {
+      case jsonParseException: JsonParseException => logger.error(jsonParseException.getMessage, jsonParseException)
+        constants.Response.JSON_PARSE_EXCEPTION.throwBaseException(jsonParseException)
+      case jsonMappingException: JsonMappingException => logger.error(jsonMappingException.getMessage, jsonMappingException)
+        constants.Response.JSON_MAPPING_EXCEPTION.throwBaseException(jsonMappingException)
+    }
+  }
+
 }
