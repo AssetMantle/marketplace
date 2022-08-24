92a9b35f5c5c65e80e4d13376f04218599b2449a
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-08-24 18:24:13 +0530

    fix: gas price UI

--- public/javascripts/blockchainTransactions/fetchPrice.js
+++ public/javascripts/blockchainTransactions/fetchPrice.js
@@ -1,11 +1,17 @@
-function getMNTLPrice() {
-    $.ajax({
-        url: "https://api.coingecko.com/api/v3/simple/price?ids=assetmantle&vs_currencies=usd",
-        type: 'GET',
-        dataType: 'json',
-        success: function(res) {
-            console.log(res);
-            alert(res);
-        }
-    });
-}
+// function getMNTLPrice() {
+//     $.ajax({
+//         url: "https://api.coingecko.com/api/v3/simple/price?ids=assetmantle&vs_currencies=usd",
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function(res) {
+//             console.log(res);
+//             alert(res);
+//         }
+//     });
+// }
+
+var getMNTLPrice = async () => {
+    const request = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=assetmantle&vs_currencies=usd");
+    const res = await request.json();
+    return (res["assetmantle"]["usd"]);
+};
