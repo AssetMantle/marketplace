1749b8b0102cb4e76a6d38b6f87a0c91c526c145
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-09-02 23:34:03 +0530

    adding load more button

--- public/javascripts/collections/loadMore.js
+++ public/javascripts/collections/loadMore.js
@@ -9,32 +9,59 @@ document.onload = function () {
     }
 }
 
+clicked = false;
+
 function loadMoreCollections() {
     const loading = document.querySelector('.loading');
-    // if ($(".noCollection").length === 0 && $(".collectionPage").length !== 0) {
     if ($(".noCollection").length === 0) {
-        let route = jsRoutes.controllers.CollectionController.collectionsPerPage($(".collectionPage").length + 1);
+        let sectionMenu = $.trim($("#sectionMenu").find(".menuItem.active").text());
+
+        let route = "";
+        let loadMore = "";
+        switch (sectionMenu) {
+            case "Art":
+                route = jsRoutes.controllers.CollectionController.collectionsPerPage($(".collectionPage").length + 1);
+                loadMore = $(".collectionsPerPage");
+                break;
+            case "Wishlist":
+                route = jsRoutes.controllers.CollectionController.wishListCollectionPerPage($(".collectionPage").length + 1);
+                loadMore = $(".collectionsPerPage");
+                break;
+            default:
+                break;
+        }
+
         $.ajax({
             url: route.url,
             type: route.type,
             async: true,
-            // global: showSpinner('recentActivity'),
             beforeSend: function () {
                 loading.classList.add('show');
+                if ($(".noCollection").length === 0) {
+                    $("#loadMoreBtnContainer").addClass("hide");
+                }
             },
             complete: function () {
                 loading.classList.remove('show');
+                if ($(".noCollection").length === 0) {
+                    $("#loadMoreBtnContainer").removeClass("hide");
+                }
             },
             statusCode: {
                 200: function (data) {
                     const loadMore = $(".collectionsPerPage");
                     loadMore.append(data);
+                    if ($(".noCollection").length !== 0) {
+                        $("#loadMoreBtnContainer").addClass("hide");
+                    }
+                    clicked = false;
                 }
             }
         });
     } else {
         console.log("NO COLLECTION LEFT")
         $(".collectionPage:last").css("margin-top", "0px");
+        $("#loadMoreBtnContainer").addClass("hide");
     }
 }
 
@@ -49,15 +76,13 @@ function getDocHeight() {
 
 collectionPageTimeout = 0;
 
-window.addEventListener('scroll', () => {
-    if ($(".collectionsPerPage").length !== 0) {
-        clearTimeout(collectionPageTimeout);
-        collectionPageTimeout = setTimeout(function () {
-            if ($(window).scrollTop() + $(window).height() >= (getDocHeight() - 10)) {
-                loadMoreCollections();
-            }
-        }, 100);
+function loadCo() {
+    if (!clicked) {
+        clicked = true;
+        loadMoreCollections();
     }
-}, {
-    passive: true
-});
+}
+
+function showLoadMoreButton(){
+    $("#loadMoreBtnContainer").removeClass("hide");
+}
