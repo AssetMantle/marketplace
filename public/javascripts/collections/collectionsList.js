9a49673d178463f0d3bf10d5c6244bd8fa7cdc23
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-09-09 20:06:13 +0530

    fixing whitelist template

--- public/javascripts/collections/collectionsList.js
+++ public/javascripts/collections/collectionsList.js
@@ -15,16 +15,19 @@ function changeActive(section) {
         case 'art':
             componentResource('collectionsPerPage', jsRoutes.controllers.CollectionController.collectionsPerPage(1));
             checkAndPushState(jsRoutes.controllers.CollectionController.viewCollections('art').url, 'art', "collections");
+            $(".loadMoreElement").addClass("show");
             showLoadMoreButton();
             break;
         case 'wishListCollections':
             componentResource('collectionsPerPage', jsRoutes.controllers.CollectionController.wishListCollectionPerPage(1));
             checkAndPushState(jsRoutes.controllers.CollectionController.viewCollections('wishListCollections').url, 'wishListCollections', "collections");
+            $(".loadMoreElement").addClass("show");
             showLoadMoreButton();
             break;
         case 'whitelist':
             componentResource('collectionsPerPage', jsRoutes.controllers.WhitelistController.whitelist());
             checkAndPushState(jsRoutes.controllers.WhitelistController.whitelist().url, '', "whitelist");
+            $(".loadMoreElement").removeClass("show");
             break;
         default:
             componentResource('collectionsPerPage', jsRoutes.controllers.CollectionController.collectionsPerPage(1));
