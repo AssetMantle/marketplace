1fc740d7b90653dae9bb10581d5858d8b167812d
avkr003 <abhinav@assetmantle.one>
2022-09-13 12:57:39 +0530

    added commonDescriptionText

---
+++ public/javascripts/form/formField.js
@@ -0,0 +1,6 @@
+function commonDescriptionInputOnKeyPress(fieldId, maximumCharacters) {
+    let field = $('#' + fieldId);
+    console.log(field.val())
+    console.log(maximumCharacters)
+    console.log(field.val().length)
+}
