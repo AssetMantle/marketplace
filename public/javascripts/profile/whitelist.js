0519acff7a936c13105fffcfba06c7ee3758f105
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-09-13 18:39:27 +0530

    fix: common epoch convertor

--- public/javascripts/profile/whitelist.js
+++ public/javascripts/profile/whitelist.js
@@ -5,18 +5,6 @@ $("#picker").daterangepicker({
     }
 });
 
-// Check Whitelist Name Exist or Not
-function checkWhitelistNameAvailable(source) {
-    const whitelistName = $(source).val();
-    if (whitelistName.length > 0 && whitelistName === "test") {
-        $("#whitelistNameAvailableError").show(300);
-        $("#whitelistName").css("border-color", "var(--error)");
-    }else{
-        $("#whitelistNameAvailableError").hide(300);
-        $("#whitelistName:focus").css("border-color","var(--dark)");
-    }
-}
-
 function showWhitelistScreen(screenID){
     if(screenID === "created"){
         $(".contentContainer .contentTitle .title .titleLabel").text("Created");
@@ -28,16 +16,6 @@ function showWhitelistScreen(screenID){
         componentResource("personalProfileContent", jsRoutes.controllers.ProfileController.joinedWhitelists(1));
     }
 }
-
-// Convert Epoch to Date and Time
-$(".tableFieldEndEpoch.epochValue, .tableFieldStartEpoch.epochValue").each(function () {
-    let seconds = $(this).text();
-    let dt = eval(seconds * 1000);
-    let date = new Date(dt);
-    let epochDate = date.toLocaleDateString();
-    let epochTime = date.toLocaleTimeString();
-    $(this).html(`${epochDate} &nbsp;${epochTime}`)
-});
 
 // Tooltip
 $(function () {
