714cf85ce9b988351b7d175686e78eebfacae7ab
Abhinav Kumar <57705190+avkr003@users.noreply.github.com>
2022-09-16 14:39:50 +0530

    Avkr003/field error (#115)
    * field level error messaging
    
    * commonInputEpochTIme
    
    * commonInputEpochTIme
    
    * commonInputEpochTIme
    
    * fix: error message UI
    
    * fix: calender menu UI
    
    * fix: form UI
    
    * check all fields filed
    
    * fix: form error field
    
    * check all fields filed
    
    Co-authored-by: chudasamajd <68367236+chudasamajd@users.noreplay.github.com>

--- app/constants/RegularExpression.scala
+++ app/constants/RegularExpression.scala
@@ -1,24 +1,29 @@
 package constants
 
+import play.api.i18n.{Messages, MessagesProvider}
+
 import scala.util.matching.Regex
 
+case class RegularExpression(name: String, regex: Regex) {
+  def getRegExErrorMessage()(implicit messagesProvider: MessagesProvider): String = Messages("REGEX_ERROR." + name)
+}
+
 object RegularExpression {
-  val ANY_STRING: Regex = """.*""".r
-  val ALL_NUMBERS_ALL_LETTERS: Regex = """^[A-Za-z0-9]*$""".r
-  val ALL_NUMBERS_ALL_CAPITAL_LETTERS: Regex = """^[A-Z0-9]*$""".r
-  val ALL_NUMBERS_ALL_SMALL_LETTERS: Regex = """^[a-z0-9]*$""".r
-  val MANTLE_ADDRESS: Regex = """^mantle[a-z0-9]{39}$""".r
-  val ALL_SMALL_LETTERS: Regex = """^[a-z]*$""".r
-  val ALL_SMALL_LETTERS_WITH_SPACE: Regex = """^[a-z ]*$""".r
-  val PASSWORD: Regex = """^[A-Za-z0-9!@#$%^&*._-]*$""".r
-  val ACCOUNT_ID: Regex = """^[a-zA-Z0-9!@#$._-]*$""".r
-  val WHITELIST_NAME: Regex = """^[a-zA-Z0-9!@#$ ._-]*$""".r
-  val MOBILE_NUMBER: Regex = """^(\+\d{1,3}[- ]?)?\d{6,14}$""".r
-  val PEG_HASH: Regex = """^[0-9]*$""".r
-  val ALL_LETTERS: Regex = """^[a-zA-z]*$""".r
-  val HASH: Regex = """^[a-fA-F0-9]*$""".r
-  val EMAIL_ADDRESS: Regex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
-  val SWIFT_CODE: Regex = """^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$""".r
-  val SPLIT: Regex = """^[0-9]+(\\.[0-9]+)?$""".r
-  val TRANSACTION_HASH: Regex = """[A-F0-9]{64}""".r
+  val ANY_STRING: RegularExpression = RegularExpression("ANY_STRING", """.*""".r)
+  val ALL_NUMBERS_ALL_LETTERS: RegularExpression = RegularExpression("ALL_NUMBERS_ALL_LETTERS", """^[A-Za-z0-9]*$""".r)
+  val ALL_NUMBERS_ALL_CAPITAL_LETTERS: RegularExpression = RegularExpression("ALL_NUMBERS_ALL_CAPITAL_LETTERS", """^[A-Z0-9]*$""".r)
+  val ALL_NUMBERS_ALL_SMALL_LETTERS: RegularExpression = RegularExpression("ALL_NUMBERS_ALL_SMALL_LETTERS", """^[a-z0-9]*$""".r)
+  val MANTLE_ADDRESS: RegularExpression = RegularExpression("MANTLE_ADDRESS", """^mantle[a-z0-9]{39}$""".r)
+  val ALL_SMALL_LETTERS: RegularExpression = RegularExpression("ALL_SMALL_LETTERS", """^[a-z]*$""".r)
+  val ALL_SMALL_LETTERS_WITH_SPACE: RegularExpression = RegularExpression("ALL_SMALL_LETTERS_WITH_SPACE", """^[a-z ]*$""".r)
+  val PASSWORD: RegularExpression = RegularExpression("PASSWORD", """^[A-Za-z0-9!@#$%^&*._-]*$""".r)
+  val ACCOUNT_ID: RegularExpression = RegularExpression("ACCOUNT_ID", """^[a-zA-Z0-9!@#$._-]*$""".r)
+  val WHITELIST_NAME: RegularExpression = RegularExpression("WHITELIST_NAME", """^[a-zA-Z0-9!@#$ ._-]*$""".r)
+  val MOBILE_NUMBER: RegularExpression = RegularExpression("MOBILE_NUMBER", """^(\+\d{1,3}[- ]?)?\d{6,14}$""".r)
+  val ALL_LETTERS: RegularExpression = RegularExpression("ALL_LETTERS", """^[a-zA-z]*$""".r)
+  val HASH: RegularExpression = RegularExpression("HASH", """^[a-fA-F0-9]*$""".r)
+  val EMAIL_ADDRESS: RegularExpression = RegularExpression("EMAIL_ADDRESS", """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r)
+  val SWIFT_CODE: RegularExpression = RegularExpression("SWIFT_CODE", """^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$""".r)
+  val SPLIT: RegularExpression = RegularExpression("SPLIT", """^[0-9]+(\\.[0-9]+)?$""".r)
+  val TRANSACTION_HASH: RegularExpression = RegularExpression("TRANSACTION_HASH", """[A-F0-9]{64}""".r)
 }
--- public/javascripts/form/formField.js
+++ public/javascripts/form/formField.js
@@ -7,3 +7,57 @@ function commonDescriptionInputOnKeyPress(fieldId, maximumCharacters) {
         $("#remainingCharacter_" + fieldId).text(maximumCharacters - inputField.val().length + "/" + maximumCharacters);
     }
 }
+
+function commonInputEpochTime(epochFieldName, fieldName, showCurrentTime) {
+    $('#' + epochFieldName).daterangepicker({
+        autoUpdateInput: showCurrentTime,
+        timePicker: true,
+        singleDatePicker: true,
+        startDate: moment().startOf('minute'),
+        drops: 'up',
+        locale: {
+            format: 'MM/DD/YYYY hh:mm A',
+            cancelLabel: 'Clear',
+            applyLabel: 'Ok'
+        }
+    });
+
+    $('#' + epochFieldName).on('apply.daterangepicker', function (ev, picker) {
+        $(this).val(picker.startDate.format('MM/DD/YYYY hh:mm A'));
+        let dateStr = picker.startDate.format('MM/DD/YYYY hh:mm A');
+        let date = new Date(dateStr);
+        const seconds = Math.floor(date.getTime() / 1000);
+        $("#epochInt_" + fieldName).val(seconds);
+        $("#epochInt_" + fieldName).trigger("keyup");
+    });
+
+    $('#' + epochFieldName).on('cancel.daterangepicker', function (ev, picker) {
+        $(this).val('');
+        $("#epochInt_" + fieldName).val('');
+        $("#epochInt_" + fieldName).trigger("keyup");
+    });
+}
+
+function setCurrentTimeForEpochField(epochFieldName, fieldName) {
+    let dateStr = moment().format('MM/DD/YYYY hh:mm A');
+    $('#' + epochFieldName).val(dateStr);
+    let date = new Date(dateStr);
+    const seconds = Math.floor(date.getTime() / 1000);
+    $("#epochInt_" + fieldName).val(seconds);
+}
+
+function clearEpochField(fieldName, showCurrentTime) {
+    if (!showCurrentTime) {
+        $("#epoch_" + fieldName).val('');
+        $("#" + fieldName).val('');
+    } else {
+        setCurrentTimeForEpochField("epoch_" + fieldName, fieldName);
+    }
+}
+
+function validateInputText(name, minimumLength, maximumLength, regex) {
+    console.log(name)
+    console.log(minimumLength)
+    console.log(maximumLength)
+    console.log(regex)
+}
