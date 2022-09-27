bee8e9d15744323cb4b7af0632f7d1b849fe5b59
unnati914 <chhabraunnati324@gmail.com>
2022-09-27 22:20:11 +0530

    added redelegatetxforms

---
+++ app/views/blockchainTransaction/companion/redelegate.scala
@@ -0,0 +1,21 @@
+package views.blockchainTransaction.companion
+
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import utilities.MicroNumber
+
+object Redelegate {
+
+  val form: Form[Data] = Form(
+    mapping(
+      constants.FormField.VALIDATOR.mapping,
+      constants.FormField.REDELEGATE_AMOUNT.mapping,
+      //constants.FormField.AVAILABLE_AMOUNT.mapping,
+      constants.FormField.GAS_AMOUNT.mapping,
+      constants.FormField.GAS_PRICE.mapping,
+      constants.FormField.PASSWORD.mapping,
+    )(Data.apply)(Data.unapply).verifying(constants.FormConstraint.redelegateConstraint))
+
+  case class Data(validator: String, redelegateAmount: MicroNumber, availableAmount: MicroNumber, gasAmount: Int, gasPrice: String, password: String)
+
+}
