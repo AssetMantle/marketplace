acc3b36a3b6524ecdfea643d80b93388aafabd30
avkr003 <abhinav@assetmantle.one>
2022-10-08 14:59:42 +0530

    Adding Notification

--- app/models/masterTransaction/Notification.scala
+++ app/models/masterTransaction/Notification.scala
@@ -3,7 +3,7 @@ package models.masterTransaction
 import java.sql.Timestamp
 import javax.inject.{Inject, Singleton}
 import models.Trait.{Entity, Logged}
-import models.common.Notification.Template
+
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json.Json
 import play.api.Logger
@@ -11,12 +11,12 @@ import models.Trait.{GenericDaoImpl, ModelTable}
 import slick.jdbc.H2Profile.api._
 import scala.concurrent.{ExecutionContext, Future}
 
-case class Notification(id: String, accountID: Option[String], template: Template, jsRoute: Option[String], read: Boolean = false, createdBy: Option[String] = None, createdOn: Option[Timestamp] = None, createdOnTimeZone: Option[String] = None, updatedOn: Option[Timestamp] = None, updatedBy: Option[String] = None, updatedOnTimeZone: Option[String] = None) extends Logged {
-  def title: String = Seq("NOTIFICATION", this.template.name, "TITLE").mkString(".")
+case class Notification(id: String, accountID: Option[String], title: String, messageParameters: Seq[String], jsRoute: Option[String], read: Boolean = false, createdBy: Option[String] = None, createdOn: Option[Timestamp] = None, createdOnTimeZone: Option[String] = None, updatedOn: Option[Timestamp] = None, updatedBy: Option[String] = None, updatedOnTimeZone: Option[String] = None) extends Logged {
+  def getTitle: String = Seq("NOTIFICATION", this.title, "TITLE").mkString(".")
 
-  def message: String = Seq("NOTIFICATION", this.template.name, "MESSAGE").mkString(".")
+  def message: String = Seq("NOTIFICATION", this.title, "MESSAGE").mkString(".")
 
-  def serialize(): Notifications.NotificationSerializable = Notifications.NotificationSerializable(id = this.id, accountID = this.accountID, template = Json.toJson(this.template).toString, jsRoute = this.jsRoute, read = this.read, createdOn = this.createdOn, createdBy = this.createdBy, createdOnTimeZone = this.createdOnTimeZone, updatedBy = this.updatedBy, updatedOn = this.updatedOn, updatedOnTimeZone = this.updatedOnTimeZone)
+  def serialize(): Notifications.NotificationSerializable = Notifications.NotificationSerializable(id = this.id, accountID = this.accountID, title = title, messageParameters = Json.toJson(this.messageParameters).toString, jsRoute = this.jsRoute, read = this.read, createdOn = this.createdOn, createdBy = this.createdBy, createdOnTimeZone = this.createdOnTimeZone, updatedBy = this.updatedBy, updatedOn = this.updatedOn, updatedOnTimeZone = this.updatedOnTimeZone)
 
 }
 
@@ -25,19 +25,21 @@ object Notifications {
 
   implicit val logger: Logger = Logger(this.getClass)
 
-  case class NotificationSerializable(id: String, accountID: Option[String], template: String, jsRoute: Option[String], read: Boolean, createdOn: Option[Timestamp], createdBy: Option[String], createdOnTimeZone: Option[String], updatedOn: Option[Timestamp], updatedBy: Option[String], updatedOnTimeZone: Option[String]) extends Entity[String] {
-    def deserialize(): Notification = Notification(id = id, accountID = accountID, template = utilities.JSON.convertJsonStringToObject[Template](template), jsRoute = jsRoute, read = read, createdOn = createdOn, createdBy = createdBy, createdOnTimeZone = createdOnTimeZone, updatedBy = updatedBy, updatedOn = updatedOn, updatedOnTimeZone = updatedOnTimeZone)
+  case class NotificationSerializable(id: String, accountID: Option[String], title: String, messageParameters: String, jsRoute: Option[String], read: Boolean, createdOn: Option[Timestamp], createdBy: Option[String], createdOnTimeZone: Option[String], updatedOn: Option[Timestamp], updatedBy: Option[String], updatedOnTimeZone: Option[String]) extends Entity[String] {
+    def deserialize(): Notification = Notification(id = id, accountID = accountID, title = title, messageParameters = utilities.JSON.convertJsonStringToObject[Seq[String]](messageParameters), jsRoute = jsRoute, read = read, createdOn = createdOn, createdBy = createdBy, createdOnTimeZone = createdOnTimeZone, updatedBy = updatedBy, updatedOn = updatedOn, updatedOnTimeZone = updatedOnTimeZone)
   }
 
   class NotificationTable(tag: Tag) extends Table[NotificationSerializable](tag, "Notification") with ModelTable[String] {
 
-    def * = (id, accountID.?, template, jsRoute.?, read, createdOn.?, createdBy.?, createdOnTimeZone.?, updatedOn.?, updatedBy.?, updatedOnTimeZone.?) <> (NotificationSerializable.tupled, NotificationSerializable.unapply)
+    def * = (id, accountID.?, title, messageParameters, jsRoute.?, read, createdOn.?, createdBy.?, createdOnTimeZone.?, updatedOn.?, updatedBy.?, updatedOnTimeZone.?) <> (NotificationSerializable.tupled, NotificationSerializable.unapply)
 
     def id = column[String]("id", O.PrimaryKey)
 
     def accountID = column[String]("accountID")
 
-    def template = column[String]("template")
+    def title = column[String]("title")
+
+    def messageParameters = column[String]("messageParameters")
 
     def jsRoute = column[String]("jsRoute")
 
@@ -73,11 +75,11 @@ class Notifications @Inject()(protected val databaseConfigProvider: DatabaseConf
 
   object Service {
 
-    def add(accountID: String, notification: constants.Notification, parameters: String*)(routeParameters: String*): Future[String] = create(Notification(id = utilities.IdGenerator.getRandomHexadecimal, accountID = Option(accountID), template = Template(name = notification.name, parameters = parameters), jsRoute = notification.route.fold[Option[String]](None)(x => Option(utilities.JsRoutes.getJsRouteString(x, routeParameters: _*)))).serialize())
+    def add(accountID: String, notification: constants.Notification, parameters: String*)(routeParameters: String*): Future[String] = create(Notification(id = utilities.IdGenerator.getRandomHexadecimal, accountID = Option(accountID), title = notification.name, messageParameters = parameters, jsRoute = notification.route.fold[Option[String]](None)(x => Option(utilities.JsRoutes.getJsRouteString(x, routeParameters: _*)))).serialize())
 
     def get(accountID: String, pageNumber: Int): Future[Seq[Notification]] = filterAndSortWithPagination(offset = (pageNumber - 1) * constants.CommonConfig.Pagination.NotificationsPerPage, limit = constants.CommonConfig.Pagination.NotificationsPerPage)(_.accountID.? === Option(accountID))(_.createdOn).map(_.map(_.deserialize()))
 
-    def add(notification: constants.Notification, parameters: String*)(routeParameters: String*): Future[String] = create(Notification(id = utilities.IdGenerator.getRandomHexadecimal, accountID = None, template = Template(name = notification.name, parameters = parameters), jsRoute = notification.route.fold[Option[String]](None)(x => Option(utilities.JsRoutes.getJsRouteString(x, routeParameters: _*)))).serialize())
+    def add(notification: constants.Notification, parameters: String*)(routeParameters: String*): Future[String] = create(Notification(id = utilities.IdGenerator.getRandomHexadecimal, accountID = None, title = notification.name, messageParameters = parameters, jsRoute = notification.route.fold[Option[String]](None)(x => Option(utilities.JsRoutes.getJsRouteString(x, routeParameters: _*)))).serialize())
 
     def getPublic(pageNumber: Int): Future[Seq[Notification]] = {
       val accountId: Option[String] = null
