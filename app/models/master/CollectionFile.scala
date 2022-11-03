17936615224d405e8be285707b1e1f839f7f25aa
avkr003 <abhinav@assetmantle.one>
2022-11-03 16:51:38 +0530

    Collection fixes

--- app/models/master/CollectionFile.scala
+++ app/models/master/CollectionFile.scala
@@ -83,10 +83,6 @@ class CollectionFiles @Inject()(
 
 
   object Service {
-    def get(id: String, documentType: String): Future[Option[CollectionFile]] = getById(id1 = id, id2 = documentType).map(_.map(_.deserialize))
-
-    def get(ids: Seq[String]): Future[Seq[CollectionFile]] = filter(_.id.inSet(ids)).map(_.map(_.deserialize))
-
     def fetchAll(): Future[Seq[CollectionFile]] = getAll.map(_.map(_.deserialize))
 
     def deleteByCollectionId(id: String): Future[Int] = deleteMultipleById1(id)
