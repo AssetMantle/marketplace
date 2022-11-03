5c60c5a3b92943d895a8df677251c527b779e170
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-11-03 17:29:20 +0530

    fix: nft creation flow

--- public/javascripts/nfts/setProperties.js
+++ public/javascripts/nfts/setProperties.js
@@ -1,19 +1,21 @@
-function saveToDraft(){
+function hideSubmitButton() {
+    $("#formSubmitButton").hide();
+}
+
+function saveToDraftNft(){
     $("#SAVE_NFT_DRAFT").attr("checked","checked");
     $("#FORM_NFT_SET_PROPERTIES_SUBMIT").click();
 }
 
-$("#FORM_NFT_SET_PROPERTIES_SUBMIT").parent().prepend(`
-                                <div class="d-flex gap-3 align-items-center me-3 propertyModalButton">
-                                    <button class="form-secondary-button active" type="button" onclick="saveToDraft()">Save Draft</button>
-                                </div>
-                                `);
+function submitButtonNft() {
+    $("#FORM_NFT_SET_PROPERTIES_SUBMIT").click();
+}
 
 function setBooleanValue(valueFieldIndex, fieldValue){
-    $(`#NFT_PROPERTIES_${valueFieldIndex}_COLLECTION_PROPERTY_NAME`).attr("value",fieldValue);
+    $(`#NFT_PROPERTIES_${valueFieldIndex}_NFT_PROPERTY_VALUE`).val(fieldValue);
 }
 
 function setNonBooleanValue(valueFieldIndex, e){
     let fieldValue = e.target.value;
-    $(`#NFT_PROPERTIES_${valueFieldIndex}_COLLECTION_PROPERTY_NAME`).attr("value",fieldValue);
+    $(`#NFT_PROPERTIES_${valueFieldIndex}_NFT_PROPERTY_VALUE`).val(fieldValue);
 }
--- public/javascripts/nfts/upload.js
+++ public/javascripts/nfts/upload.js
@@ -1,4 +1,20 @@
+let hashData = "";
+let collectionId = "";
+
 function onSuccessfulUpload(fileHash) {
     $('#nftFileHashName').text(fileHash);
-    setFileName(fileHash);
+    const index = fileHash.lastIndexOf('/');
+    hashData = fileHash.slice(index + 1);
+    setFileName();
+}
+
+function passCollectionId(id){
+    collectionId = id;
+}
+
+function setFileName(){
+    if(hashData !== "" && collectionId !== ""){
+        $("#uploadNftNextButton").removeClass("disable");
+        $("#uploadNftNextButton").attr("onclick",`getForm(jsRoutes.controllers.NFTController.basicDetailsForm("${collectionId}","${hashData}"))`);
+    }
 }
