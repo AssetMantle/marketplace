a09b16c2e25dbfcb0a286907143387d7790a3dee
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-11-15 21:04:43 +0530

    fix: wishlist nft on scroll

--- public/javascripts/profile/wishlist/nftLoadMore.js
+++ public/javascripts/profile/wishlist/nftLoadMore.js
@@ -11,8 +11,16 @@ document.onload = function () {
 
 function loadMoreNFTs(accountId, collectionId) {
     const loading = document.querySelector('.loading');
+    let totalNfts = $(".singleNFTCard").length;
+    let draftNFT = $('.draftNft').length;
+    let createCard = $(".createNFTCard").length;
+    let totalWishlist = (totalNfts - createCard - draftNFT);
+    let pageNumber = Math.floor(totalWishlist / 6) + 1;
+    if (totalWishlist < (pageNumber * 6) && totalWishlist > ((pageNumber - 1) * 6) && totalWishlist % 6 !== 0) {
+        pageNumber = pageNumber + 1;
+    }
     if ($(".noNFT").length === 0) {
-        let route = jsRoutes.controllers.WishlistController.collectionNFTsPerPage(accountId, collectionId, $(".nftsPerPage").length + 1);
+        let route = jsRoutes.controllers.WishlistController.collectionNFTsPerPage(accountId, collectionId, pageNumber);
         $.ajax({
             url: route.url,
             type: route.type,
