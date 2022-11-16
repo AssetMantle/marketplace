9a72d04a296a72109f474f6dd9a00055470d581b
Jagdish Chudasama <chudasamajagdish12@gmail.com>
2022-11-16 13:47:45 +0530

    Jagdish/sales UI (#244)
    * fix: messages
    
    * fix: set empty collection message
    
    * fix: removing container space
    
    * fix: add nft link in offers
    
    * fix: offered nft modal icon
    
    * feat: adding menu in offers
    
    * fix: drop down menu ui
    
    * fix: remove created collection empty box
    
    * fix: container width
    
    * fix: menu width
    
    Co-authored-by: chudasamajd <68367236+chudasamajd@users.noreplay.github.com>

--- public/javascripts/profile/offer/loadMore.js
+++ public/javascripts/profile/offer/loadMore.js
@@ -8,3 +8,16 @@ document.onload = function () {
         window.scrollTo(0, 0);
     }
 }
+
+function showOfferScreen(screenID) {
+    switch (screenID) {
+        case "Made":
+            $(".contentContainer .contentTitle .title .titleLabel").text("Made");
+            $(".contentContainer .contentTitle .titleMenu").show();
+            break;
+        case "Received":
+            $(".contentContainer .contentTitle .title .titleLabel").text("Received");
+            $(".contentContainer .contentTitle .titleMenu").hide();
+            break;
+    }
+}
