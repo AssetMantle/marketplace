a05f3d0fbeac3e097624fe7babbdb0270d2abcc9
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-11-21 14:08:35 +0530

    fix: no of nft field UI

--- public/javascripts/sale/createCollectionSale.js
+++ public/javascripts/sale/createCollectionSale.js
@@ -13,3 +13,18 @@ function onCollectionSelect(collectionId, accountId) {
         }
     });
 }
+
+$("#SELECT_COLLECTION_ID_field .custom-option").on("click", function() {
+    let collectionId = $(this).data("value");
+    let userId = $(".userName").text();
+    componentResource('collectionOwnedNFTs', jsRoutes.controllers.CollectionController.countAccountNFTs(`${collectionId}`,`${userId}`))
+});
+
+function setSaleNFTValue(e){
+    let fieldValue = e.target.value;
+    let totalOwnedNFT = $("#collectionOwnedNFTs").text();
+    $(`#SALE_NFT_NUMBER`).val(fieldValue);
+    if(fieldValue > totalOwnedNFT){
+        $("#FORM_COLLECTION_SALE_SUBMIT").addClass("disable");
+    }
+}
