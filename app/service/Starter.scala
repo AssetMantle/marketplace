102d6d4508b4ef3048bac379632e3abf897ed929
avkr003 <abhinav@assetmantle.one>
2023-04-18 18:33:27 +0530

    BugBountyFix 2

--- app/service/Starter.scala
+++ app/service/Starter.scala
@@ -301,8 +301,8 @@ class Starter @Inject()(
 
     val owners = Seq("sanidhya17", "hattori", "iamsecure1920", "ibrahim005", "eysec", "sey1")
 
-    Await.result(masterNFTs.Service.delete("371ce239b88e770f86ea6393503b5b3ffeeb131f5a09e709758cc93f596abe91"), Duration.Inf)
     Await.result(masterNFTOwners.Service.deleteByNFTID("371ce239b88e770f86ea6393503b5b3ffeeb131f5a09e709758cc93f596abe91"), Duration.Inf)
+    Await.result(masterNFTs.Service.delete("371ce239b88e770f86ea6393503b5b3ffeeb131f5a09e709758cc93f596abe91"), Duration.Inf)
 
     val bronzeFileHash = utilities.FileOperations.getFileHash(bronzeFile)
     val newFileName = bronzeFileHash + ".gif"
