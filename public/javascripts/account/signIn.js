109546a7222a99a2b93cbbed48fc59d08f03bd8e
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2023-08-20 13:01:29 +0530

    feat: adding reCaptcha

--- public/javascripts/account/signIn.js
+++ public/javascripts/account/signIn.js
@@ -1,3 +1,5 @@
+reCaptchaFlag = false;
+
 function showPassword() {
     let password = $('#signInPassword')[0];
     if (password.type === "password") {
@@ -8,5 +10,36 @@ function showPassword() {
         password.type = "password";
         $(".closeEye").removeClass("hidden");
         $(".openEye").addClass("hidden");
+    }
+}
+
+$(document).ready(function () {
+    $('#FORM_SIGN_IN_WITH_CALLBACK_SUBMIT').addClass("disable");
+})
+
+function activeButton() {
+    let signInUsername = $("#signInUsername").val();
+    let signInPassword = $("#signInPassword").val();
+    if (signInUsername !== "" && signInPassword !== "" && reCaptchaFlag === true) {
+        $("#FORM_SIGN_IN_WITH_CALLBACK_SUBMIT").removeClass("disable");
+    } else {
+        $("#FORM_SIGN_IN_WITH_CALLBACK_SUBMIT").addClass("disable");
+    }
+}
+
+$("#signInUsername, #signInPassword").on("input", function()
+{
+    activeButton();
+});
+
+var onSubmit = function(token) {
+    reCaptchaFlag = true;
+    activeButton();
+};
+
+<!-- Google reCaptcha -->
+function CaptchaCallback() {
+    if ( $('#myRecaptcha').length ) {
+        grecaptcha.render('myRecaptcha', {'sitekey' : '6LfhFrwnAAAAAKKOVgEAdCHHtgalQj0PosHPn8Xu','callback' : onSubmit});
     }
 }
